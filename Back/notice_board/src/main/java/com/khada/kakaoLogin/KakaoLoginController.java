068f74f53d8444e3aef51a0168f85de288fb4134
hyunkang-lee <klun102w@gmail.com>
2024-04-10 22:37:09 +0900

    카카오 로그인 준비

---
+++ Back/notice_board/src/main/java/com/khada/kakaoLogin/KakaoLoginController.java
@@ -0,0 +1,8 @@
+package com.khada.kakaoLogin;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class KakaoLoginController {
+
+}
